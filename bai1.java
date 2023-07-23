91471661f1da612909c0924f50bb8fdf93efe52d
DoGiaBao <baodo3010@gmail.com>
2023-07-23 20:20:33 +0700

    chua bai 1

---
+++ bai1.java
@@ -0,0 +1,13 @@
+package solution;
+
+public class bai1 {
+    public static void main(String[] args) {
+        int[] array = {2, 7, 11, 15};
+        int index1 = 0;
+        int index2 = 1; // the index of the second element
+        int sum = array[index1] + array[index2];
+        System.out.println("The sum of elements at index " + index1 + " and " + index2 + " is " + sum);
+    }
+}
+
+    
